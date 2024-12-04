67c2fd82cbebc4675b56820d3d9aca4c02cd6a49
rochamanuela <rochadesouzamanuela@gmail.com>
2024-12-04 11:06:06 -0300

    feat: responsiveness login and registration

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -13,26 +13,29 @@ export default function Login() {
             </div>
             <div className="main-container-content">
                 <div className="logotype"><a href="/">FN Book</a></div>
-                <h1>Login</h1>
-                <p className="bem-vindo">Bem vindo(a) de volta! Faça login para continuar</p>
-                <form className="formulario" method="POST" action="">
-                    <label htmlFor="email">Email</label>
-                    <Input
-                        inputType="text"
-                        inputId="email"
-                        inputName="email"
-                        inputPlaceholder="name@example.com"
-                    />
-                    <label htmlFor="senha">Senha</label>
-                    <Input
-                        inputType="password"
-                        inputId="senha"
-                        inputName="senha"
-                        inputPlaceholder=""
-                    />
-                    <input type="submit" value="Entrar" className="buttonLogin" />
-                </form>
-                <p className="main-container-content-link">Não possui uma conta ainda? <a href="/register">Crie uma gratuitamente</a></p>
+
+                <div className="main-container-section">
+                    <h1 style={{marginTop: 0}}>Login</h1>
+                    <p className="bem-vindo">Bem vindo(a) de volta! Faça login para continuar</p>
+                    <form className="formulario" method="POST" action="">
+                        <label htmlFor="email">Email</label>
+                        <Input
+                            inputType="text"
+                            inputId="email"
+                            inputName="email"
+                            inputPlaceholder="name@example.com"
+                        />
+                        <label htmlFor="senha">Senha</label>
+                        <Input
+                            inputType="password"
+                            inputId="senha"
+                            inputName="senha"
+                            inputPlaceholder=""
+                        />
+                        <input type="submit" value="Entrar" className="buttonLogin" />
+                    </form>
+                    <p className="main-container-content-link">Não possui uma conta ainda? <a href="/register">Crie uma gratuitamente</a></p>
+                </div>
             </div>
         </div>
     )
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -13,53 +13,55 @@ export default function Register() {
       </div>
       <div className="main-container-content">
         <div className="logotype"><a href="/">FN Book</a></div>
-        <h1>Cadastro</h1>
-        <p className="bem-vindo">Bem vindo(a)! É um prazer ter você conosco</p>
-        <form className="formulario" method="POST" action="">
-          <label htmlFor="nome">Nome completo</label>
-          <Input
-            inputType="text"
-            inputId="nome"
-            inputName="nome"
-            inputPlaceholder="Mario dos Santos"
-          />
-          <label htmlFor="email">Email</label>
-          <Input
-            inputType="text"
-            inputId="email"
-            inputName="email"
-            inputPlaceholder="name@example.com"
-          />
-          <div className="container-data-foto">
-            <div className="input-group">
-              <label htmlFor="data-nasc">Data de nascimento</label>
-              <Input
-                inputType="date"
-                inputId="data-nasc"
-                inputName="data-nasc"
-                inputPlaceholder=""
-              />
+        <div className="main-container-section">
+          <h1>Cadastro</h1>
+          <p className="bem-vindo">Bem vindo(a)! É um prazer ter você conosco</p>
+          <form className="formulario" method="POST" action="">
+            <label htmlFor="nome">Nome completo</label>
+            <Input
+              inputType="text"
+              inputId="nome"
+              inputName="nome"
+              inputPlaceholder="Mario dos Santos"
+            />
+            <label htmlFor="email">Email</label>
+            <Input
+              inputType="text"
+              inputId="email"
+              inputName="email"
+              inputPlaceholder="name@example.com"
+            />
+            <div className="container-data-foto">
+              <div className="input-group">
+                <label htmlFor="data-nasc">Data de nascimento</label>
+                <Input
+                  inputType="date"
+                  inputId="data-nasc"
+                  inputName="data-nasc"
+                  inputPlaceholder=""
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="foto-perfil">Foto de perfil</label>
+                <Input
+                  inputType="file"
+                  inputId="foto-perfil"
+                  inputName="foto-perfil"
+                  inputPlaceholder=""
+                />
+              </div>
             </div>
-            <div className="input-group">
-              <label htmlFor="foto-perfil">Foto de perfil</label>
-              <Input
-                inputType="file"
-                inputId="foto-perfil"
-                inputName="foto-perfil"
-                inputPlaceholder=""
-              />
-            </div>
-          </div>
-          <label htmlFor="senha">Senha</label>
-          <Input
-            inputType="password"
-            inputId="senha"
-            inputName="senha"
-            inputPlaceholder=""
-          />
-          <input type="submit" value="Entrar" className="buttonLogin" />
-        </form>
-        <p className="main-container-content-link">Já possui uma conta? <a href="/login">Faça login aqui</a></p>
+            <label htmlFor="senha">Senha</label>
+            <Input
+              inputType="password"
+              inputId="senha"
+              inputName="senha"
+              inputPlaceholder=""
+            />
+            <input type="submit" value="Entrar" className="buttonLogin" />
+          </form>
+          <p className="main-container-content-link">Já possui uma conta? <a href="/login">Faça login aqui</a></p>
+        </div>
       </div>
     </div>
   )
